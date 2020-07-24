24baa216790746da0bf1871c494e8134fedf8b03
Mukesh Soni <msony552@gmail.com>
2020-07-24 15:41:16 +0530

    Create FlutterMapCodeSnippet.dart
    Code Snippet for the medium article

---
+++ FlutterMapCodeSnippet.dart
@@ -0,0 +1,21 @@
+return Container(
+  child: Row(
+  childern: <Widget>[
+      Icon(
+      Icons.add,
+      size: 25.0,
+      color: Colors.green,
+      ),
+      Icon(
+      Icons.search,
+      size: 25.0,
+      color: Colors.blue,
+      ),
+      Icon(
+      Icons.close,
+      size: 25.0,
+      color: Colors.red,
+      ),
+    ],
+  ),
+)
